c5e5b15eda01e7b6280e84ae0d33e979e36b3f22
Onur <ob-vest@hotmail.com>
2023-06-05 23:50:44 +0200

    added new countdown and removed dependency

--- components/CountdownTimer.vue
+++ components/CountdownTimer.vue
@@ -1,61 +1,83 @@
 <template>
-  <!-- Needs to be evaluated on the client side, and not SSR -->
-  <ClientOnly>
-    <VueCountdown
-      :time="time"
-      :interval="100"
-      v-slot="{ days, hours, minutes, seconds }"
+  <div
+    class="w-full select-none rounded-b-2xl from-black to-transparent bg-gradient-to-t via-black pb-2"
+  >
+    <div
+      v-if="new Date(releaseDate) > new Date()"
+      class="flex justify-center gap-4 w-full"
     >
-      <div
-        class="select-none rounded-b-2xl flex justify-center gap-4 w-full from-black to-transparent bg-gradient-to-t via-black pb-2"
-      >
-        <div class="w-fit h-10 text-center">
-          <h4 class="font-bold">{{ days }}</h4>
-          <p>DAYS</p>
-        </div>
-        <div class="w-fit h-10 text-center">
-          <h4 class="font-bold">{{ hours }}</h4>
-          <p>HOURS</p>
-        </div>
-        <div class="w-fit h-10 text-center">
-          <h4 class="font-bold">{{ minutes }}</h4>
-          <p>MINS</p>
-        </div>
-        <div class="w-fit h-10 text-center">
-          <h4 class="font-bold">{{ seconds }}</h4>
-          <p>SECS</p>
-        </div>
+      <div class="text-center w-fit h-12">
+        <h4 class="font-bold text-xl">{{ days }}</h4>
+        <div class="text-sm text-gray-400">DAYS</div>
       </div>
-      <!-- New Year Countdown：{{ days }} days, {{ hours }} hours,
-      {{ minutes }} minutes, {{ seconds }}.{{ Math.floor(milliseconds / 100) }}
-      seconds. -->
-    </VueCountdown>
-  </ClientOnly>
+      <div class="text-center w-fit h-12 mx-2">
+        <h4 class="font-bold text-xl">{{ hours }}</h4>
+        <div class="text-sm text-gray-400">HOURS</div>
+      </div>
+      <div class="text-center w-fit h-12">
+        <h4 class="font-bold text-xl">{{ minutes }}</h4>
+        <div class="text-sm text-gray-400">MIN</div>
+      </div>
+      <div class="text-center w-fit h-12 mx-2">
+        <h4 class="font-bold text-xl">{{ seconds }}</h4>
+        <p class="text-sm text-gray-400">SEC</p>
+      </div>
+    </div>
+    <h2 v-else class="text-center">The wait is over!</h2>
+  </div>
 </template>
-
-<script>
-import VueCountdown from "@chenfengyuan/vue-countdown";
+<script lang="ts">
+interface Countdown {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 export default {
-  components: {
-    VueCountdown,
-  },
   props: {
     releaseDate: {
-      type: Date,
+      type: String,
       required: true,
     },
   },
-  data() {
-    return {
-      time: this.remainingTime(),
+  setup(props) {
+    const countdown: Countdown = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
     };
-  },
-  methods: {
-    remainingTime() {
+
+    const days = ref(countdown.days.toString().padStart(2, "0"));
+    const hours = ref(countdown.hours.toString().padStart(2, "0"));
+    const minutes = ref(countdown.minutes.toString().padStart(2, "0"));
+    const seconds = ref(countdown.seconds.toString().padStart(2, "0"));
+
+    const updateCountdown = () => {
       const now = new Date();
-      return this.releaseDate - now;
-    },
+      const releaseDate = new Date(props.releaseDate);
+      const diff = releaseDate.getTime() - now.getTime();
+      const daysValue = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hoursValue = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutesValue = Math.floor((diff / 1000 / 60) % 60);
+      const secondsValue = Math.floor((diff / 1000) % 60);
+      days.value = daysValue.toString().padStart(2, "0");
+      hours.value = hoursValue.toString().padStart(2, "0");
+      minutes.value = minutesValue.toString().padStart(2, "0");
+      seconds.value = secondsValue.toString().padStart(2, "0");
+    };
+    updateCountdown();
+    setInterval(() => {
+      updateCountdown();
+    }, 1000);
+
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
   },
 };
 </script>
@@ -65,3 +87,45 @@ p {
   @apply text-zinc-300 text-sm font-light;
 }
 </style>
+
+<!-- <script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    releaseDate: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  },
+  mounted() {
+    this.updateCountdown();
+    setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  },
+  methods: {
+    updateCountdown() {
+      const now = new Date();
+      const releaseDate = new Date(this.releaseDate);
+      const diff = releaseDate.getTime() - now.getTime();
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / 1000 / 60) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+      this.days = days.toString().padStart(2, "0");
+      this.hours = hours.toString().padStart(2, "0");
+      this.minutes = minutes.toString().padStart(2, "0");
+      this.seconds = seconds.toString().padStart(2, "0");
+    },
+  },
+});
+</script> -->
--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -1,19 +1,22 @@
 <template>
-  <div
-    class="hover:scale-110 transition-transform btn-gradient-1 w-fit mx-auto"
-  >
+  <div>
     <div
-      class="rounded-2xl bg-[url('/matrix-template.png')] bg-cover bg-no-repeat bg-blend-multiply bg-black bg-opacity-60"
+      class="hover:scale-110 transition-transform btn-gradient-1 w-full h-56 sm:w-64 sm:h-44 mx-auto"
     >
-      <div class="w-64 h-44 justify-center flex flex-col items-center">
-        <div class="my-auto text-center">
-          <h2 class="text-2xl text-center font-semibold line-clamp-2 mx-2">
-            {{ item.headline }}
-          </h2>
-          <p class="text-zinc-300">{{ item.subheadline }}</p>
-        </div>
+      <div
+        class="w-full h-full rounded-2xl bg-[url('https://firebasestorage.googleapis.com/v0/b/everycountdown-888.appspot.com/o/Apple-WWDC-2023.png?alt=media&token=319adf96-5ae0-44c8-a7ac-e68175480bb4')] bg-cover bg-no-repeat bg-blend-multiply bg-black bg-opacity-60"
+        @error="handleError"
+      >
+        <div class="w-full h-full justify-center flex flex-col items-center">
+          <div class="my-auto text-center">
+            <h2 class="text-2xl text-center font-semibold line-clamp-2 mx-2">
+              {{ item.headline }}
+            </h2>
+            <p class="text-zinc-300">{{ item.subheadline }}</p>
+          </div>
 
-        <CountdownTimer :releaseDate="new Date(item.release_date)" />
+          <CountdownTimer :releaseDate="item.release_date" />
+        </div>
       </div>
     </div>
   </div>
@@ -27,10 +30,20 @@ export default {
       required: true,
     },
   },
+  methods: {
+    handleError(event: Event) {
+      const backgroundImage = event.target as HTMLDivElement;
+      backgroundImage.style.backgroundImage = `url('/bg-fallback.png')`;
+    },
+  },
 };
 </script>
 <style scoped>
 /* method 1 -> use border-image */
+.bg {
+  background-image: url("https://firebasestorage.googleapis.com/v0/b/everycountdown-888.appspot.com/o/Apple-WWDC-2023.png?alt=media&token=319adf96-5ae0-44c8-a7ac-e68175480bb4"),
+    url("/bg-fallback.png");
+}
 .btn-gradient-1 {
   /* Border */
   border: 0.5px solid transparent;
