ce9beb81fc9235b61174089172b5c759b223f003
Onur <ob-vest@hotmail.com>
2023-06-09 03:49:29 +0200

    added privacy policy

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,17 +5,18 @@
         <ul
           class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"
         >
-          <li>
-            <a href="#" class="mr-4 hover:text-white md:mr-6">About</a>
-          </li>
-          <li>
-            <a href="#" class="mr-4 hover:text-white md:mr-6">Privacy Policy</a>
-          </li>
           <!-- <li>
-            <a href="#" class="mr-4 hover:text-white md:mr-6">Licensing</a>
+            <a href="#" class="mr-4 hover:text-white md:mr-6">About</a>
           </li> -->
           <li>
-            <a href="#" class="hover:text-white">Contact</a>
+            <nuxt-link to="/privacy-policy">
+              <a class="mr-4 hover:text-white md:mr-6">Privacy Policy</a>
+            </nuxt-link>
+          </li>
+          <li>
+            <a href="mailto: everycountdown@invoke.dk" class="hover:text-white"
+              >Contact</a
+            >
           </li>
         </ul>
       </div>
