a6bf188f75483bd3c893e141d181e73b2d75c371
Onur <ob-vest@hotmail.com>
2023-06-09 03:49:56 +0200

    added privacy policy for realzz

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="space-y-5 mx-5">
+    <h1>Privacy Policy for EveryCountdown</h1>
+    <p>Effective Date: June 9, 2023</p>
+
+    <p>
+      EveryCountdown ("we," "us," or "our") is committed to protecting the
+      privacy of our users. This Privacy Policy outlines how we collect, use,
+      disclose, and protect your personal information when you use our mobile
+      application, EveryCountdown.
+    </p>
+
+    <h2>1. Information We Collect:</h2>
+    <p>
+      EveryCountdown does not collect any personally identifiable information
+      (PII) or personal data from its users. We do not require you to create an
+      account, provide your name, email address, or any other personal
+      information to use our application. Therefore, we do not store or process
+      any user data on our servers.
+    </p>
+
+    <h2>2. Non-Personal Information:</h2>
+    <p>
+      When you use EveryCountdown, we may collect non-personal information that
+      does not directly identify you. This may include technical information
+      such as your device type, operating system, unique device identifiers, and
+      usage statistics. This information is collected solely for the purpose of
+      improving our application and providing a better user experience.
+    </p>
+
+    <h2>3. Third-Party Services:</h2>
+    <p>
+      EveryCountdown does not integrate any third-party services or analytics
+      providers that collect personal information from our users. We do not
+      share any user data with third parties for marketing, advertising, or any
+      other purposes.
+    </p>
+
+    <h2>4. Cookies and Similar Technologies:</h2>
+    <p>
+      EveryCountdown does not use cookies or similar tracking technologies to
+      collect any user information.
+    </p>
+
+    <h2>5. Changes to this Privacy Policy:</h2>
+    <p>
+      We may update this Privacy Policy from time to time to reflect changes in
+      our practices or legal obligations. We encourage you to review this
+      Privacy Policy periodically to stay informed about how we collect, use,
+      and protect your information.
+    </p>
+
+    <h2>6. Contact Us:</h2>
+    <p>
+      If you have any questions, concerns, or requests regarding this Privacy
+      Policy or our practices, please contact us at
+      <a href="mailto:everycountdown@invoke.dk">everycountdown@invoke.dk</a>.
+    </p>
+
+    <p>
+      By using EveryCountdown, you signify your acceptance of this Privacy
+      Policy. If you do not agree with this policy, please do not use our
+      application.
+    </p>
+
+    <p>
+      Please note that this Privacy Policy applies solely to EveryCountdown and
+      does not govern any third-party websites, services, or applications that
+      may be linked from our application. We encourage you to review the privacy
+      policies of any third-party services you interact with.
+    </p>
+  </div>
+</template>
